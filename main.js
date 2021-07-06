bde44648159a5c8d5d8885dd6faf7588627c4a99
Candy Crush <76581343+Aadya-ai@users.noreply.github.com>
2021-07-06 12:41:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+song_1 = "";
+song_2 = "";
+
+function preload() {
+    song_1 = loadSound("OFFICIAL MUSIC VIDEO _ BABY DON’T WANT YOU _ Baby Queen _ Rimorav Vlogs presents RI Vlogs [YTmp3.net].mp3");
+    song_2 = loadSound("OFFICIAL MUSIC VIDEO _ VAARI JAWAN _ Rimorav Vlogs [YTmp3.net].mp3")
+}
+
+function setup() {
+    canvas = createCanvas(550, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function AI_MUSIC() {
+    window.location.href = "";
+}
+
+function Click_here() {
+    window.location.href = "";
+}
