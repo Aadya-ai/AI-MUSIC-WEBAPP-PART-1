45eac34eea470aa9751be0118da6c911b057a543
Candy Crush <76581343+Aadya-ai@users.noreply.github.com>
2021-07-10 14:06:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 song_1 = "";
 song_2 = "";
 
-leftWristX = 0;
-leftWristY = 0;
-
-rightWristX = 0;
-rightWristY = 0;
-
 function preload() {
     song_1 = loadSound("OFFICIAL MUSIC VIDEO _ BABY DON’T WANT YOU _ Baby Queen _ Rimorav Vlogs presents RI Vlogs [YTmp3.net].mp3");
     song_2 = loadSound("OFFICIAL MUSIC VIDEO _ VAARI JAWAN _ Rimorav Vlogs [YTmp3.net].mp3")
@@ -18,39 +12,90 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
-    
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+	poseNet.on('pose', gotPoses);
 }
 
-function gotPoses(results) {
-    if(results.length > 0) {
-        console.log(results);
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
 
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+song1_status = "";
+song2_status = "";
 
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
-    }
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
 }
 
 function draw() {
-    image(video, 0, 0, 600, 500);
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#00008B");
+	stroke("#00008B");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+		}
+	}
+
 }
 
-function play() {
-    song_1.play();
-    song_1.setVolume();
-    song_1.rate();
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
 }
 
-function AI_MUSIC() {
-    window.location.href = "https://aadya-ai.github.io/AI-MUSIC-WEBAPP-PART-2/";
-}
-
-function Click_here() {
-    window.location.href = "https://aadya-ai.github.io/AI-MUSIC-WEBAPP-PART-3/";
+function stop() {
+    song.stop();
 }
